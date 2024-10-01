aae453237e1e9d45576f56ff77c7cd866a448e75
Ariful Islam Shawon <33293851+AiShawonBD@users.noreply.github.com>
2024-10-01 21:37:25 +0600

    Update the basic issue

--- Basic/01_hello.sh
+++ Basic/01_hello.sh
@@ -2,11 +2,13 @@
 #Author : Ariful Islam Shawon
 #Creation Date: 3 Mar 2024
 #Singel Line Comment Exam
+#Version  : 1.2
 
 << Comments
     This is Multi Line
-    Commenty Example
-Comments
+    Comment Example
+Comments 
 
 echo "Hello Welcome to Bash Screpting Learing"
-echo "Hello Bash Screpting World" 
+echo "Hello Bash Screpting World"
+echo "Hello Bash Screpting World"
